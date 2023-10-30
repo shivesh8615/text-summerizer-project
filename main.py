b9b704c74f65393213b7343dfbfee6a90a1af660
shivesh8615 <shivraj8615@gmail.com>
2023-10-30 17:12:29 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-"""from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+"""from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline"""
 from textSummarizer.logging import logger
@@ -19,7 +19,7 @@ except Exception as e:
 
 
 
-"""STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
@@ -30,7 +30,7 @@ except Exception as e:
         raise e
 
 
-
+"""
 STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -69,5 +69,3 @@ except Exception as e:
         raise e
 
 """
-
-
